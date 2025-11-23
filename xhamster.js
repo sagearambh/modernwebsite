54b1c5dd44e39ac23ee2a4f692a82d2805769d6b
yash <ymandlekar9826@gmail.com>
2025-11-23 15:26:27 +0530

    chore: Remove `xhamster.js` and outdated URLs, and refine thumbnail hover behavior.

--- xhamster.js
+++
@@ -1,8 +0,0 @@
-[
-  {
-    url: "https://video7.xhcdn.com/key=Wft08O6udFmYviBFAZ9NxA,end=1750878000,limit=3/data=212.8.243.133-dvp/speed=0/006/088/754/1080p.h264.mp4",
-  },
-  {
-    url: "https://ip411581239.ahcdn.com/key=E-+iXv1cBsmH8fI91PBwWw,s=,end=1750881600,limit=3/data=212.8.243.133-dvp/state=aFwnKgB8gAAAA8QAAAAA/reftag=0201380214/ssd2/21/6/262393926/021/439/758/0p.h264.mp4",
-  },
-];
